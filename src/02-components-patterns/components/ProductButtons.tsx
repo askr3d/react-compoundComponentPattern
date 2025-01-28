8b257174c16306314787937b46d6fc54b36ace49
askr3d <asesino9393@gmail.com>
2025-01-28 16:38:34 -0600

    Reapply "state initializer + fn child, render props"
    This reverts commit a56dff8ee3dc20bb47e70d82f35e0ed616ba3caa.

--- src/02-components-patterns/components/ProductButtons.tsx
+++ src/02-components-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useContext } from "react";
+import { CSSProperties, useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
 import styles from '../styles/styles.module.css';
@@ -9,7 +9,16 @@ export type Props = {
 }
 
 export const ProductButtons = ({ className, style }: Props) => {
-    const { increaseBy, counter } = useContext(ProductContext)
+    //TODO: maxCount
+    const { increaseBy, counter, maxCount } = useContext(ProductContext)
+
+    //TODO: isMaxReached = useCallback, dependencias [ count, maxCount ]
+
+    const isMaxReached = useCallback(
+      () => !!maxCount && counter === maxCount,
+      [counter],
+    );
+    
 
     return (
         <div 
@@ -22,7 +31,8 @@ export const ProductButtons = ({ className, style }: Props) => {
             >-</button>
             <div className={styles.countLabel}>{ counter }</div>
             <button 
-                className={ styles.buttonAdd }
+                className={` ${styles.buttonAdd} ${isMaxReached() && styles.disabled} `}
+                disabled={isMaxReached()}
                 onClick={() => increaseBy(+1)}
             >+</button>
         </div>
--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -1,28 +1,44 @@
-import { useEffect, useState } from "react";
-import { onChangeArgs, Product } from "../types";
+import { useEffect, useRef, useState } from "react";
+import { InitialValues, onChangeArgs, Product } from "../types";
 
 interface useProductArgs {
   product: Product
   onChange?: (args: onChangeArgs) => void
   value?: number
+  initialValues?: InitialValues
 }
 
-export const useProduct = ({onChange, product, value=0}: useProductArgs) => {
-    const [counter, setCounter] = useState(value);
+export const useProduct = ({onChange, product, value=0, initialValues}: useProductArgs) => {
+    const [counter, setCounter] = useState(initialValues?.count || value);
+    const isMounted = useRef(false);
 
     const increaseBy = (value: number) => {
-        const newValue = Math.max( counter + value, 0 );
-        setCounter(newValue);
+        let newValue = Math.max( counter + value, 0 );
+        if (initialValues?.maxCount) {
+          newValue = Math.min(newValue, initialValues.maxCount);
+        }
+        
 
+        setCounter(newValue);
         onChange && onChange({ count: newValue, product });
     }
 
+    const reset = () => {
+      setCounter(initialValues?.count || value);
+    }
+
     useEffect(() => {
+      if ( !isMounted.current ) return;
+      else isMounted.current = true;
+
       setCounter( value );
-    }, [value])
+    }, [value]);
     
   return {
+    counter,
+    isMaxCountReached: !!initialValues?.maxCount && initialValues.maxCount === counter,
+    maxCount: initialValues?.maxCount,
     increaseBy,
-    counter
+    reset,
   }
 }
